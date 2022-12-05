8a483690e7466bb9efce98fff9124399b315c768
boonloong <obl0702@gmail.com>
2022-12-05 22:51:30 +0800

    add "explore top book" component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { ExploreTopBooks } from './layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
 import {Navbar} from './layouts/NavbarAndFooter/Navbar';
+
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+      <ExploreTopBooks/>
+    </div>
+    
   );
 }
 
