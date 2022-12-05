decf2c68acfb5f0089420e661e2f96a3e5e01f08
boonloong <obl0702@gmail.com>
2022-12-05 22:38:48 +0800

    created homepage navigation bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import {Navbar} from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Fullstack Application with Springboot and React</h1>
-    </div>
+    <Navbar/>
   );
 }
 
