62e576aaf44b7e6335d0fc5b96f2b9c9f9322194
boonloong <obl0702@gmail.com>
2022-12-13 11:09:59 +0800

    add starreview component

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { StartsReview } from "../Utils/StarsReview";
 
 export const BookCheckoutPage = () => {
     
@@ -74,6 +75,7 @@ export const BookCheckoutPage = () => {
                             <h2>{book?.title}</h2>
                             <h5 className="text-primary">{book?.author}</h5>
                             <p className="lead">{book?.description}</p>
+                            <StartsReview rating={4} size={32}/>
                         </div>
                     </div>
                 </div>
@@ -93,6 +95,7 @@ export const BookCheckoutPage = () => {
                         <h2>{book?.title}</h2>
                         <h5 className="text-primary">{book?.author}</h5>
                         <p className="lead">{book?.description}</p>
+                        <StartsReview rating={4} size={32}/>
                     </div>
                 </div>
             </div>
